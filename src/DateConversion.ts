c8dc7a61710b3c785ebe900e9cf6ba43cb975f22
Bastian Hodapp <bastianhodapp@gmail.com>
2021-12-23 15:47:24 +0100

    make method actually work

--- src/DateConversion.ts
+++ src/DateConversion.ts
@@ -1,15 +1,29 @@
 export function dateToDateTabString(date: Date) {
-    const dayNumber = date.getDate();
+    const dayNumber = date.getDay();
 
-    // const prefix: string = "tab-";
+    const prefix: string = "tab-";
 
-    // let suffix: string;
+    let suffix: string;
 
     switch (dayNumber) {
         case 1:
-
+            suffix = "mon";
             break;
+        case 2:
+            suffix = "tue";
+            break;
+        case 3:
+            suffix = "wed";
+            break;
+        case 4:
+            suffix = "thu";
+            break;
+        case 5:
+            suffix = "fri";
+            break;
+        default:
+            return null;
     }
 
-    return "test";
+    return prefix + suffix;
 }
